3a98f9e79e3e5a38b871bca0e5058ec36102b54d
tyler <ortizt89@gmail.com>
2019-04-06 15:02:55 -0400

    Add aspnet core project and solution for scraper demo. Setup gitignore. Configure a basic MVC page with link to planned api route. Added api route

---
+++ ScraperDemo/Controllers/ScraperController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ScraperDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ScraperController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return "Web scraper demo";
+        }
+    }
+}
